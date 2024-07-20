3055cd0e7f2293c8a59c59cf7cb947f705cada1f
Thakur Satyam <satyam.singh@gogroup.co>
2024-07-21 00:29:46 +0530

    intial

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,24 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Careers",
+    newTab: false,
+    path: "/careers",
+  },
+  {
+    id: 2.1,
+    title: "About us",
+    newTab: false,
+    path: "/aboutus",
+  },
+];
+
+export default menuData;
